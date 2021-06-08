3486ae7b31609d65162a692cd219ddd2c4c6b93b
seeker <populpock2@gmail.com>
2021-06-09 00:36:14 +0300

    Vector Introduced (0.7)

--- Lab10.h
+++ Lab10.h
@@ -2,10 +2,18 @@
 #define LAB10_H
 
 #include <stdio.h>
-char** parser(char* filename);
-int count_words(FILE* fp);
+
+typedef struct {
+    char** arr;
+    size_t size;
+    size_t capacity;
+} vector_t;
+
+vector_t* parser(char* filename);
+void print_vector(vector_t* str);
 char* readWord(FILE* fp);
-void quicksort(char** str, int length);
+void quicksort(char** str, int high);
+void push_back(vector_t* str, char* word);
 void swap(char** a, char** b);
 int is_ch(char ch);
 
